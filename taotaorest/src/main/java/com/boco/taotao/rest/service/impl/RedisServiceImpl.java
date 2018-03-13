b6019d523c051b32b8fd2ee109a8ab480b7763e6
Sheamus <zhoujunfeng@boco.com.cn>
2018-03-13 18:00:04 +0800

    增加扫描的文件路径，注入成功

--- taotaorest/src/main/java/com/boco/taotao/rest/service/impl/RedisServiceImpl.java
+++ taotaorest/src/main/java/com/boco/taotao/rest/service/impl/RedisServiceImpl.java
@@ -1,8 +1,10 @@
 package com.boco.taotao.rest.service.impl;
 
+import com.boco.taotao.rest.dao.JedisClient;
 import com.boco.taotao.rest.service.RedisService;
 import com.boco.taotao.util.ExceptionUtil;
 import com.boco.taotao.vo.TaotaoResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisServiceImpl implements RedisService {
 
-    /*@Autowired
-    private JedisClientSingle jedisClient;*/
+    @Autowired
+    private JedisClient jedisClient;
 
     @Value("${INDEX_CONTENT_REDIS_KEY}")
     private String INDEX_CONTENT_REDIS_KEY;
@@ -21,7 +23,7 @@ public class RedisServiceImpl implements RedisService {
     @Override
     public TaotaoResult syncContent(long categoryId) {
         try {
-            /*jedisClient.hdel(INDEX_CONTENT_REDIS_KEY, categoryId + "");*/
+            jedisClient.hdel(INDEX_CONTENT_REDIS_KEY, categoryId + "");
         } catch (Exception e) {
             e.printStackTrace();
             return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
